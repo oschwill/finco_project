cb3c4a41e4c37aa4ce8d155cb54ea801d3e844b1
Melina <melina.schild@icloud.com>
2024-02-21 19:56:30 +0100

    balanceIcons component & added to reports

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-linear": "linear-gradient(180deg, #44BBFE 0%, #1E78FE 100%)",
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: { primary : "#44BBFE"}
-    },
-  },
-  plugins: [require("daisyui")],
+  // theme: {
+  //   extend: {
+  //     backgroundImage: {
+  //       "gradient-linear": "linear-gradient(180deg, #44BBFE 0%, #1E78FE 100%)",
+  //       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+  //       "gradient-conic":
+  //         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  //     },
+  //     colors: { primary : "#44BBFE"}
+  //   },
+  // },
+  // plugins: [require("daisyui")],
 };
 export default config;
