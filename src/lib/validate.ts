6ad602eecf545a08165e9fc5acca71041ac5b74b
oschwill <o.schwill@web.de>
2024-02-25 15:53:31 +0100

    auth / design undso

---
+++ src/lib/validate.ts
@@ -0,0 +1,10 @@
+import Joi from 'joi';
+
+export const registerAccountSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'de'] } })
+    .required(),
+  password: Joi.string().min(8).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  terms: Joi.string().valid('terms').required(),
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        inputBorderColor: '#EBEBEB',
+        inputBackColor: '#F7F7F7',
+        buttonBgGradientFrom: '#00d4ff',
+        buttonBgGradientTo: '#1E78FE',
+        uploadButtonGradientFrom: '#FFCF53',
+        uploadButtonGradientTo: '#FF9900',
+        linkColor: '#1E78FE',
       },
     },
   },
